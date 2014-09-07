0d23b6a7095751d2017ed9e49e7fec3700f3771c
Nosajool <jmloo@uwaterloo.ca>
2014-09-07 12:08:27 -0400

    post request

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Reddit Anonymizer' });
+});
+
+router.post('/anonymize', function(req, res){
+	res.send(req.body);
 });
 
 module.exports = router;
