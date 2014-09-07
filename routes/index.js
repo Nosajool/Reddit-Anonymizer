f424e04ff8e22ba8dbdb4aba3fbb5e1fb458bdf0
Nosajool <jmloo@uwaterloo.ca>
2014-09-07 12:58:54 -0400

    include comment authors

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ var unirest = require('unirest');
 
 var MAX_NUMBER_OF_COMMENTS = 5;
 
+function Comment(user, body){
+	this.user = user;
+	this.body = body;
+}
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Reddit Anonymizer' });
@@ -12,24 +17,26 @@ router.get('/', function(req, res) {
 router.post('/anonymize', function(req, res){
 	unirest.get(req.body.reddit_url + ".json")
 		.end(function(response){
+
+			var thread = response.body[0].data.children[0].data;
+			var comment_data = response.body[1].data;
+
 			if(response.error){
 				res.send("Invalid URL");
 			}
 			else{
-				var num_comments = response.body[0].data.children[0].data.num_comments;
+				var num_comments = thread.num_comments;
 				var comments = [];
-				console.log("Number of Comments: " + num_comments);
+
 				if(num_comments > MAX_NUMBER_OF_COMMENTS){
-					console.log("Only taking 5 comments");
 					num_comments = MAX_NUMBER_OF_COMMENTS;
 				}
-				else{
-					console.log("we good");
-				}
+
 				for(var i = 0; i < num_comments; i++){
-					console.log(i);
-					comments.push(response.body[1].data.children[i].data.body);
+					var comment = new Comment(comment_data.children[i].data.author, comment_data.children[i].data.body);
+					comments.push(comment);
 				}
+
 				res.render('index', { title: 'Reddit Anonymizer', comments: comments});
 			}
 		});
