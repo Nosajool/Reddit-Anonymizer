ba37f93bdbde8a9f1c7ac98c4f7454b660761523
Nosajool <jmloo@uwaterloo.ca>
2014-09-07 12:21:17 -0400

    unirest request works

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -7,7 +8,17 @@ router.get('/', function(req, res) {
 });
 
 router.post('/anonymize', function(req, res){
-	res.send(req.body);
+
+	unirest.get(req.body.reddit_url + ".json")
+		.end(function(response){
+			if(response.error){
+				res.send("Invalid URL");
+			}
+			else{
+				console.log(response.body);
+				res.send(response.body);
+			}
+		});
 });
 
 module.exports = router;
