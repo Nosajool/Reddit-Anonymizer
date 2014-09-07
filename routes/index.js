2aba931bd19fa82d0f0c6e532128c22766576c5f
Nosajool <jmloo@uwaterloo.ca>
2014-09-07 14:24:45 -0400

    Nested comments work

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,24 @@ var express = require('express');
 var router = express.Router();
 var unirest = require('unirest');
 
-var MAX_NUMBER_OF_COMMENTS = 5;
+var MAX_NUMBER_OF_CONVERSATIONS = 5;
 
 function Comment(user, body){
 	this.user = user;
 	this.body = body;
 }
+
+function Conversation(){
+	this.comments = [];
+}
+
+Conversation.prototype.addComment = function(user, body){
+	this.comments.push(new Comment(user, body));
+};
+
+Conversation.prototype.getComments = function(){
+	return this.comments;
+};
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -17,27 +29,37 @@ router.get('/', function(req, res) {
 router.post('/anonymize', function(req, res){
 	unirest.get(req.body.reddit_url + ".json")
 		.end(function(response){
-
-			var thread = response.body[0].data.children[0].data;
-			var comment_data = response.body[1].data;
-
 			if(response.error){
 				res.send("Invalid URL");
 			}
 			else{
-				var num_comments = thread.num_comments;
-				var comments = [];
+				var thread = response.body[0].data.children[0].data;
+				var comment_data = response.body[1].data;
+				var num_conversations = thread.num_comments;
+				var conversations = [];
 
-				if(num_comments > MAX_NUMBER_OF_COMMENTS){
-					num_comments = MAX_NUMBER_OF_COMMENTS;
+				if(num_conversations > MAX_NUMBER_OF_CONVERSATIONS){
+					num_conversations = MAX_NUMBER_OF_CONVERSATIONS;
 				}
 
-				for(var i = 0; i < num_comments; i++){
-					var comment = new Comment(comment_data.children[i].data.author, comment_data.children[i].data.body);
-					comments.push(comment);
+				for(var i = 0; i < num_conversations; i++){
+					var conversation = new Conversation();
+
+					// push the first comment
+					var currentComment = comment_data.children[i].data;
+					conversation.addComment(currentComment.author, currentComment.body);
+					// push the first comment of the children
+					while(typeof(currentComment.replies.data) != "undefined"){
+						console.log("got here");
+						currentComment = currentComment.replies.data.children[0].data;
+						conversation.addComment(currentComment.author, currentComment.body);
+					}
+
+					conversations.push(conversation);
+					console.log(conversation);
 				}
 
-				res.render('index', { title: 'Reddit Anonymizer', comments: comments});
+				res.render('index', { title: 'Reddit Anonymizer', conversations: conversations});
 			}
 		});
 });
